eb392983512e5e36e6e4ed3f25112c3bc1a00cf8
MooreLyndon <74111054+MooreLyndon@users.noreply.github.com>
2020-12-14 17:25:21 +0000

    Add files via upload

---
+++ Homeworkdue1412/src/homeworkdue1412/Homeworkdue1412.java
@@ -0,0 +1,40 @@
+
+package homeworkdue1412;
+
+
+import java.util.Scanner;
+
+public class Homeworkdue1412 {
+
+
+    public static void main(String[] args) {
+
+      Scanner input = new Scanner(System.in);
+      int ten = 10;
+      
+      
+          long kilobytes = 1024;
+    long megabytes = kilobytes * 1024;
+
+
+
+    System.out.println("Total memory (megabytes): " + 
+        Runtime.getRuntime().totalMemory() / (float) megabytes);
+         System.out.println("Free memory (megabytes): " + 
+        Runtime.getRuntime().freeMemory() / (float) megabytes);
+     
+    }
+ 
+    }
+    
+       
+        
+        
+        
+        
+        
+        
+        
+    
+    
+
